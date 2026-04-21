efa7cd1317698fcd02afb7fe188916e77fee23e7
Mohamed khaja eshaq <mdsohel46940@gmail.com>
2026-04-21 22:52:14 +0530

    ff

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
 
-int main() {
-    int block[5]={100,500,200,300,600};
-    int process[4]={212,417,112,426};
-    int i,j;
+int main()
+{
+    int b[10], p[10], i, j, nb, np;
 
-    for(i=0;i<4;i++) {
-        for(j=0;j<5;j++) {
-            if(block[j]>=process[i]) {
-                printf("Process %d allocated to block %d\n",i+1,j+1);
-                block[j]-=process[i];
+    printf("Enter number of blocks: ");
+    scanf("%d", &nb);
+
+    printf("Enter block sizes:\n");
+    for(i=0;i<nb;i++)
+        scanf("%d",&b[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d",&np);
+
+    printf("Enter process sizes:\n");
+    for(i=0;i<np;i++)
+        scanf("%d",&p[i]);
+
+    // -------- FIRST FIT --------
+    printf("\nFirst Fit:\n");
+
+    for(i=0;i<np;i++)
+    {
+        for(j=0;j<nb;j++)
+        {
+            if(b[j] >= p[i])
+            {
+                printf("P%d allocated to Block %d\n", i, j);
+                b[j] -= p[i];
                 break;
             }
         }
     }
+
+    // -------- BEST FIT --------
+    printf("\nBest Fit:\n");
+
+    for(i=0;i<np;i++)
+    {
+        int best = -1;
+
+        for(j=0;j<nb;j++)
+        {
+            if(b[j] >= p[i])
+            {
+                if(best == -1 || b[j] < b[best])
+                    best = j;
+            }
+        }
+
+        if(best != -1)
+        {
+            printf("P%d allocated to Block %d\n", i, best);
+            b[best] -= p[i];
+        }
+    }
+
+    return 0;
 }
